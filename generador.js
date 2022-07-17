cfe20ceffbf1e110d9331f187f78d0d9f1352237
Joe Colino <joepc74@gmail.com>
2022-07-17 11:38:03 +0200

    añadida web con texto y numero de paginas

--- generador.js
+++ generador.js
@@ -18,29 +18,32 @@ const operanciones = [
         ajuste: (a,b) => [a,b]
     }];
 
-    function randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    
-    function genera() {
-        var item = operanciones[Math.floor(Math.random()*operanciones.length)];
-        nro1=randomIntFromInterval(item.nro1[0],item.nro1[1]);
-        nro2=randomIntFromInterval(item.nro2[0],item.nro2[1]);
-        [nro1,nro2]=item.ajuste(nro1,nro2);
-        return [nro1,nro2,item.simbolo];
-    }
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
+function generanros() {
+    var item = operanciones[Math.floor(Math.random()*operanciones.length)];
+    nro1=randomIntFromInterval(...item.nro1);
+    nro2=randomIntFromInterval(...item.nro2);
+    [nro1,nro2]=item.ajuste(nro1,nro2);
+    return [nro1,nro2,item.simbolo];
+}
+
+function generador(pages=1) {
+    if (isNaN(pages)) pages=1;
+    console.log(pages);
     var doc = new jspdf.jsPDF({orientation: "portrait",unit: "mm", format:"a4"});
     let widthhpage=doc.internal.pageSize.getWidth()-5
 
     doc.setFont("Arial","bold");
     doc.setFontSize(35);
     
-    for (let page=0; page<10;page++) {
+    for (let page=0; page<pages;page++) {
         let x=35;
         let y=20;
         for (let i=0;i<40;i++) {
-            [nro1,nro2,simbolo]=genera();
+            [nro1,nro2,simbolo]=generanros();
             
             doc.text(nro1.toString(), x, y,{'align':'right'});
             doc.text(simbolo+" "+nro2.toString(), x, y+10,{'align':'right'});
@@ -55,3 +58,4 @@ const operanciones = [
     }
     doc.deletePage(11)
     doc.save(`mates.pdf`);
+}
